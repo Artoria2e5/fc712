d18f475611b1a7d026ecae49bae251b47933c6f8
Mingye Wang <arthur200126@gmail.com>
2021-04-06 19:24:05 +0800

    Update drawing1.py

--- 91/drawing1.py
+++ 91/drawing1.py
@@ -73,6 +73,7 @@ for y in range(10, 111, 10):
 # task 3 & 7
 # no i don't wanna make it optional
 
+
 def make_grid(x0: num, y0: num, xn: int, yn: int, w: num, h: num, **kw) -> List[int]:
     ret = []
     for ix in range(0, xn + 1):
@@ -121,4 +122,49 @@ def create_ngon_differently(x0: num, y0: num, n: num, l: num, **kw):
 
 
 create_ngon_differently(100, 100, 3, 40, fill="pink")
+
+# task 7a
+def make_a_golden_spiral(x0: num, y0: num, n: num, a: num, dt: num, **kw) -> List[int]:
+    ret = []
+    golden_ratio = (1 + math.sqrt(5)) / 2
+    prev_p = (x0, y0)
+    for i in range(1, n + 1):
+        t = dt * i
+        r = a * math.e ** (golden_ratio * t)
+        d = (math.cos(t) * r, math.sin(t) * r)
+        curr_p = (x0 + d[0], y0 + d[1])
+        ret.append(create_line(*prev_p, *curr_p, **kw))
+        prev_p = curr_p
+
+
+make_a_golden_spiral(100, 100, 20, 1, math.pi / 12, fill="#808080")
+
+# task 7b
+def create_nstar(
+    x1: num, y1: num, x2: num, y2: num, n: int, delta: float = 0.0, **kw
+) -> List[int]:
+    """
+    No I am not supposed to duplicate code but i did anyways
+
+    :param delta: angle offset
+    """
+    rets = []
+    cx = (x1 + x2) / 2
+    cy = (y1 + y2) / 2
+    rx = (x2 - x1) / 2
+    ry = (y2 - y1) / 2
+    all_points = [
+        oval_point_for_angle(cx, cy, rx, ry, 2 * math.pi / n * i + delta)
+        for i in range(0, n)
+    ]
+    for i in range(0, n):
+        for j in range(0, n):
+            if i == j:
+                continue
+            rets.append(create_line(*all_points[i], *all_points[j], **kw))
+    return rets
+
+
+create_nstar(70, 70, 100, 100, 5, fill="blue")
+
 complete()
