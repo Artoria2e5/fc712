1b9b4f9d409990d1c7e7f7de0e271eb78a9b885f
Mingye Wang <arthur200126@gmail.com>
2021-04-10 06:25:28 +0800

    Create misc.js

---
+++ misc.js
@@ -0,0 +1,13 @@
+
+// nth row of the pascal triangle
+const binom_row = function(n) {
+    let row = [1, 1];
+    for (let i = 1; i < n; i++) {
+        let newrow = [1];
+        for (let j = 1; j <= row.length; j++) {
+            newrow[j] = (row[j] || 0) + row[j - 1];
+        }
+        row = newrow;
+    }
+    return row;
+}
